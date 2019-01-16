7f429d2ac7e09c6f72ad92b1ca0e0b515a2fbea2
Adelino Junior <alfjuniorbh.web@gmail.com>
2019-01-16 11:16:03 -0200

    Title site

--- resources/views/site/layout/site.blade.php
+++ resources/views/site/layout/site.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sicoob - Cartão Premiado</title>
+    <title>Sicoob - Campeonato Mineiro 2019</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Sicoob" />
     <meta name="keywords" content="Sicoob, Sicoobcard, Sicoob Consórcios" />
