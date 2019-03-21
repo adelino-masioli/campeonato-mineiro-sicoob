8e4be1fb294291ebaa6ddab557da09842b86f673
alfjuniorbh <alfjuniorbh.web@gmail.com>
2019-03-21 16:08:07 -0300

    Show quartas

--- resources/views/site/partials/section-five-staging.blade.php
+++ resources/views/site/partials/section-five-staging.blade.php
@@ -28,24 +28,32 @@
 
                         <div class="row">
                                 @foreach(\App\Round::getRouds($game->id) as $round)
-                                <div class="col-xs-12 col-sm-12 col-md-6 col-md-offset-3 text-center">
+                                <div class="col-xs-12 col-sm-12 col-md-{{$game->order > 11 ? '8' : '6'}} col-md-offset-{{$game->order > 11 ? '2' : '3'}} text-center">
                                     <div class="row">
                                        
                                         <div class="col-xs-12 col-md-12 animated animate-box">
                                                 <small class="datetimeround">{{$round->date}}</small>
                                         </div>
                                                         
-                                        <div class="col-xs-5 col-md-5 showres animate-box">
+                                        <div class="col-md-{{$game->order > 11 ? '3' : '5'}} showres animate-box">
                                                 <img src="{{asset('escudos/'.$round->teamA->flag)}}" class="img-responsive" alt="{{$round->teamA->name}}">
                                                 <p class="nametime">{{$round->teamA->name}}</p>
                                                 <span class="placar">{{$round->result_a}}</span>
                                         </div>
-                                        <div class="col-xs-2 col-md-2 showres showresx animate-box"><h2>X</h2></div>
-                                        <div class="col-xs-5 col-md-5 showres animate-box">
+                                        <div class="col-xs-2 col-md-2 showres showresx animate-box" style="{{$game->order > 11 ? 'min-height: 100px;' : ''}}"><h2>X</h2></div>
+                                        <div class="col-md-{{$game->order > 11 ? '3' : '5'}} showres animate-box">
                                                 <img src="{{asset('escudos/'.$round->teamB->flag)}}" class="img-responsive" alt="{{$round->teamB->name}}">
                                                 <p class="nametime">{{$round->teamB->name}}</p>
                                                 <span class="placar">{{$round->result_b}}</span>
                                         </div>
+
+                                        @if($game->order > 11)
+                                                <div class="hidden-xs col-md-4 teamlocal animated">                          
+                                                        {{$round->city}}<br/>
+                                                        {{$round->stadium}}
+                                                </div>
+                                        @endif
+
                                     </div>
                                 </div>
                                 @endforeach
